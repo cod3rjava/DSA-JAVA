17dede65623514118e4554b913f7032c1273768b
cod3rjava <cod3rjava@gmail.com>
2023-05-26 19:24:08 +0530

    added linkedlist code - appendAtLast-removeAtLast

---
+++ coding/src/com/cod3rjava/linklist/LinkedListTest.java
@@ -0,0 +1,110 @@
+package com.cod3rjava.linklist;
+
+public class LinkedListTest {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public int getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+            return 0;
+        }
+        return this.head.value;
+    }
+
+    public int getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+            return 0;
+        }
+        return this.tail.value;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void printList() {
+        Node temp = head;
+//        if(head == tail){
+//            System.out.println(head.value);
+//            return;
+//        }
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public void removeOtherApproch() {
+        if (length == 0) {
+            return;
+        }
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            Node temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            tail = temp;
+            tail.next = null;
+        }
+        length--;
+    }
+
+    public Node remove() {
+        if (length == 0) {
+            return null;
+        }
+        if (head == tail) {
+            head = tail = null;
+        }
+        Node temp = head;
+        Node pre = head;
+        while(temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public LinkedListTest(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+
+    }
+
+
+
+    class Node {
+        private int value;
+        private Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+}
---
+++ coding/src/com/cod3rjava/linklist/Main.java
@@ -0,0 +1,27 @@
+package com.cod3rjava.linklist;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        LinkedListTest linkedListTest = new LinkedListTest(4);
+
+//        System.out.println(linkedListTest.getHead());
+//        System.out.println(linkedListTest.getTail());
+//        System.out.println(linkedListTest.getLength());
+//        System.out.println("\n Linked List");
+        linkedListTest.printList();
+        System.out.println("\n ---------------------------------------");
+        linkedListTest.append(6);
+        linkedListTest.append(3);
+        linkedListTest.append(61);linkedListTest.append(45);
+        linkedListTest.remove();
+        linkedListTest.printList();
+        System.out.println("\n ---------------------------------------");
+    }
+
+
+
+
+
+}
